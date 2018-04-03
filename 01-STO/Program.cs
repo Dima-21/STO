b83f89e3c2bb1abcabf83f89f9bd8f2e7d8fa967
Dima <Dima@DESKTOP-QVVB42L>
2018-04-03 22:00:15 +0300

    Add project files.

---
+++ 01-STO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_STO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
