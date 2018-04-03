11002e5b8fee1a0c2442b36a90ee5cc61a2c183f
Dima <Dima@DESKTOP-QVVB42L>
2018-04-03 23:03:58 +0300

    Rev.1.0

---
+++ 01-STO/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_STO
+{
+    class Car
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public Car()
+        {
+            Name = "Unknown";
+            Year = 2000;
+        }
+
+        public Car(string n, int y)
+        {
+            Name = n;
+            Year = y;
+        }
+    }
+}
---
+++ 01-STO/STO.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_STO
+{
+    class STO : IEnumerable
+    {
+        List<Car> Cars = new List<Car>();
+        List<Car> Repair = new List<Car>();
+        List<Car> Finish = new List<Car>();
+        public void AddCar(Car c)
+        {
+            Cars.Add(c);
+        }
+
+        public void RepairCar()
+        {
+            Repair.Add(Cars[0]);
+            Cars.RemoveAt(0);
+        }
+        public void FinishRepair()
+        {
+            Finish.Add(Repair[0]);
+            Repair.RemoveAt(0);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Iteration(this);
+        }
+
+        struct Iteration
+        {
+            int lst;
+            int ar;
+            Car[][] arr = new Car[3][];          
+            public object Current
+            {
+                get
+                {
+                    return car[lst][ar];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                ;
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
